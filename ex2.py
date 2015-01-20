2ea70f37948cb74f08666286a62660476cce913b
Your name <parthibhank82@gmail.com>
2015-01-20 22:34:00 +0530

    Add fix

--- ex2.py
+++ ex2.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+
 img = cv2.imread('Car.jpg', 0)
 cv2.imshow('car', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-cv2.imwrite('Car.png', img)
+print 'Saving the image as png'
+cv2.imwrite('Car.png',img)
+
+    
