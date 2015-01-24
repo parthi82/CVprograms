06f1908079eb6e6694eea0c42473161a98247469
Your name <parthibhank82@gmail.com>
2015-01-24 20:18:30 +0530

    add 3-d array

--- numpy_02.py
+++ numpy_02.py
@@ -1,5 +1,5 @@
 import numpy as np 
-# 2-d array
+# 2-d arrays
 b = np.array([[1,2,3], [4,7,8]])
 print 'b:'
 print b
@@ -11,6 +11,29 @@ print 'length of first dimension :'
 print len(b)
 
 b = np.array([[2,4,6,8], [1,3,5,7], [7,9,6,8]])
+print 'b:'
+print b
+print 'Dimension = '
+print b.ndim
+print 'Shape :'
+print b.shape
+print 'length of first dimension :'
+print len(b)
+
+print
+
+# 3-d arrays
+b = np.array([ [[1], [2]], [[1], [3]] ])
+print 'b:'
+print b
+print 'Dimension = '
+print b.ndim
+print 'Shape :'
+print b.shape
+print 'length of first dimension :'
+print len(b)
+
+b= np.array([ [[1,2,3],[3,5,7]], [[2,5,6], [6,6,6]] ])
 print 'b:'
 print b
 print 'Dimension = '
