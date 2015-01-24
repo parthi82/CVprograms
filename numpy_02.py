1ce484e36e25b4b8d53538894d7a4d34872af21d
Your name <parthibhank82@gmail.com>
2015-01-24 20:01:59 +0530

    Add 2-d array

---
+++ numpy_02.py
@@ -0,0 +1,21 @@
+import numpy as np 
+# 2-d array
+b = np.array([[1,2,3], [4,7,8]])
+print 'b:'
+print b
+print 'Dimension = '
+print b.ndim
+print 'Shape :'
+print b.shape
+print 'length of first dimension :'
+print len(b)
+
+b = np.array([[2,4,6,8], [1,3,5,7], [7,9,6,8]])
+print 'b:'
+print b
+print 'Dimension = '
+print b.ndim
+print 'Shape :'
+print b.shape
+print 'length of first dimension :'
+print len(b)
