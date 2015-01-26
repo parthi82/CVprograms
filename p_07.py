52a3377c58bc18e0a34e690a54a020890f58a7f6
Your name <parthibhank82@gmail.com>
2015-01-26 19:52:38 +0530

    Add pixel manipulation

--- p_07.py
+++ p_07.py
@@ -11,3 +11,24 @@ print 'The data type of the Image array : '
 print img.dtype
 print 'The length of the first dimension of the Image : '
 print len(img)
+print 'pixel at (89,139) has BGR value : '
+px = img[89,139] 
+print px
+print 'Pixel at (53,76) has BGR value : '
+px = img[53,76]
+print px
+print 'The Blue value at (53,76) is '
+print img [53,76,0]
+print '''After changing the value of blue to 255,
+         The pixel at (53,76) becomes : '''
+img[53,76,0] = 255
+print img[53,76]
+
+for i in range(40,52):
+	for j in range(63):
+		img[i,j,0] = 255
+
+print 'Yes, see the image !'     
+cv2.imshow('Car', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
