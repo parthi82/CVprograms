57a6ee93294d5804f140a6482fede43e7b3e84a3
Your name <parthibhank82@gmail.com>
2015-01-28 22:33:37 +0530

    Add merge

--- p_09.py
+++ p_09.py
@@ -6,4 +6,8 @@ b, g, r = cv2.split(img)
 print 'The blue channel.....'
 cv2.imshow('Red Channel', r)
 cv2.waitKey(2800)
-
+# merging
+img2 = cv2.merge((b,g,r))
+print 'merged image'
+cv2.imshow('merged image....', img2)
+cv2.waitKey(2800)
