ecd0259ad10b9c0889672d6a201f2cd40a2acc1d
Your name <parthibhank82@gmail.com>
2015-01-28 22:38:25 +0530

    Add numpy numpy solution

--- p_09.py
+++ p_09.py
@@ -11,3 +11,14 @@ img2 = cv2.merge((b,g,r))
 print 'merged image'
 cv2.imshow('merged image....', img2)
 cv2.waitKey(2800)
+
+# more efficient way using numpy
+b = img[:, :, 0]
+g = img[:, :, 1]
+r = img[:, :, 2]
+
+# chage all blue values to 255 and red values to 0
+img[:, :, 0] = 255
+img[:, :, 2] = 0
+cv2.imshow('New', img)
+cv2.waitKey(2800)
