b133495591dc75bfd4deb4c49fffcc9ac64ac808
Your name <parthibhank82@gmail.com>
2015-02-03 21:40:44 +0530

    Add comment

--- p_10.py
+++ p_10.py
@@ -5,11 +5,12 @@ s, img = cam.read()
 winName = "Movement Indicator"
 cv2.namedWindow(winName, cv2.CV_WINDOW_AUTOSIZE)
 
+# Capture and show frames till ESC Key is pressed
 while s:
     cv2.imshow( winName,img )
     s, img = cam.read()
     key = cv2.waitKey(10)
     if key == 27:
         cv2.destroyWindow(winName)
-        break
+        break        
 print "Goodbye"
