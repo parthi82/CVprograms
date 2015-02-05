d886b2a5cd30d55ddbc16555b06a0bff85646a64
K.Parthibhan <parthi82@users.noreply.github.com>
2015-02-05 23:20:15 +0530

    Add comments

--- ex2.py
+++ ex2.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-
+# load image as grayscale
 img = cv2.imread('Car.jpg', 0)
 cv2.imshow('car', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 print 'Saving the image as png'
+# sve image as a PNG image
 cv2.imwrite('Car.png',img)
 
     
